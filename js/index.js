19ebb29471fa653cc97180298fdbbf4ce24fbdeb
Tomas Barisone <tomasbarisone16@gmail.com>
2024-01-27 14:06:45 -0300

    Inico del proyecto y descripcion del mismo, estructura del HTML

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/* 
+
+Proyecto Academico (Primera Pre-Entrega)
+
+En esta primera pre-entrega el proyecto consiste en un programa que permita que el usuario pueda ingresar una serie de alumnos y sus respectivas notas para luego calcular su nota final de cursada.
+Ademas, el programa permitirá saber:
+- Si el alumno promocionó la materia.
+- Si aprobo la cursada pero tiene que rendir un final.
+- Si tiene que rendir recuperatorio.
+- Si tiene que recursar la materia.
+
+*/
+
+
+
