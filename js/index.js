0a66c88c646384af56ead829b799530b0dada303
Tomas Barisone <tomasbarisone16@gmail.com>
2024-02-14 21:33:14 -0300

    correccion js

--- js/index.js
+++ js/index.js
@@ -96,20 +96,3 @@ alumnosDesaprobados().forEach(alumno => console.log(alumno.nombre + " " + alumno
 
 console.log("Los alumnos que recursaron son:")
 alumnosRecursados().forEach(alumno => console.log(alumno.nombre + " " + alumno.apellido))
-
-/* for (let i = 0; i < alumnos; i++){
-    nombre = prompt("Ingrese el nombre completo del alumno");
-    nota1 = parseFloat(prompt(`Ingrese la nota del primer examen de ${nombre}`));
-    nota2 = parseFloat(prompt(`Ingrese la nota del segundo examen de ${nombre}`));
-    if ((nota1 >= 0 && nota1 < 4) && (nota2 >= 0 && nota2 < 4)){
-        console.log(`El alumno ${nombre} debe recursar la materia`)
-    } else if (((nota1 >= 4 && nota1 < 7) && (nota2 >= 4)) || ((nota2 >= 4 && nota2 < 7) && (nota1 >= 4))){
-        console.log(`El alumno ${nombre} aprobó la cursada pero tiene que rendir un examen final`)
-    } else if (((nota1 >= 0 && nota1 < 4) && (nota2 >= 4)) || ((nota2 >= 0 && nota2 < 4) && (nota1 >= 4))){
-        console.log(`El alumno ${nombre} debe rendir recuperatorio`)
-    } else {
-        console.log(`El alumno ${nombre} promocionó la materia`)
-    }
-    promedio(nota1, nota2);
-} */
-
